48d26ed7bcf9913c1bf1ad84ad2d130454708274
Lyndon Panton <LyndonPanton@gmail.com>
2025-04-16 14:56:01 +0100

    Added meta information

---
+++ basic/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="application-name" content="Portfolio" />
+    <meta name="author" content="Lyndon Mykal Panton" />
+    <meta name="description" content="A software engineering portfolio
+        containing developer information, work experience, projects, and more" />
+        <meta name="keywords" content="computer science, software,
+        software engineer, software developer, software engineering, software
+        development, web development, frontend, backend, fullstack, client-side,
+        server-side, database" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0
+            user-scalable=true, minimum-scale=0.25, maximum-scale=4.0" />
+    <title>Lyndon Mykal Panton | Software Engineer</title>
+    <link rel="stylesheet" href="./css/styles.css" />
+    <script src="./js/script.js"></script>
+</head>
+<body>
+    <aside>
+        
+    <aside>
+
+    <header>
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
