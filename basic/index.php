87f52f9d73dbf6ee966167f78dc0122afa506270
Lyndon Panton <LyndonPanton@gmail.com>
2025-04-16 15:07:50 +0100

    Imported and applied external font

--- basic/index.php
+++ basic/index.php
@@ -12,7 +12,13 @@
         server-side, database" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0
             user-scalable=true, minimum-scale=0.25, maximum-scale=4.0" />
+
     <title>Lyndon Mykal Panton | Software Engineer</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&family=Rethink+Sans:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
+    
     <link rel="stylesheet" href="./css/styles.css" />
     <script src="./js/script.js"></script>
 </head>
